0937ab761daeb7341d3d46dae389d2f8dc5d72b4
basil <vanderman789@gmail.com>
2025-12-25 23:14:36 +0300

    add class Attach and update TestBase

---
+++ src/test/java/helpers/Attach.java
@@ -0,0 +1,48 @@
+package helpers;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.nio.charset.StandardCharsets;
+
+import static com.codeborne.selenide.Selenide.getWebDriverLogs;
+import static org.openqa.selenium.logging.LogType.BROWSER;
+
+public class Attach {
+
+    @Attachment(value = "{attachName}", type = "image/png", fileExtension = "png")
+    public static byte[] screenshotAs(String attachName) {
+        return ((TakesScreenshot) WebDriverRunner.getWebDriver())
+                .getScreenshotAs(OutputType.BYTES);
+    }
+
+    @Attachment(value = "Page source", type = "text/html", fileExtension = "html")
+    public static byte[] pageSource() {
+        return WebDriverRunner.getWebDriver()
+                .getPageSource().getBytes(StandardCharsets.UTF_8);
+    }
+
+    @Attachment(value = "Browser console logs", type = "text/plain", fileExtension = "txt")
+    public static String consoleLogs() {
+        return String.join("\n", getWebDriverLogs(BROWSER));
+    }
+
+    @Attachment(value = "Video", type = "text/html", fileExtension = "html")
+    public static String addVideo() {
+        return "<html><body><video width='100%' height='100%' controls autoplay>" +
+                "<source src='" + getVideoUrl() + "' type='video/mp4'>" +
+                "</video></body></html>";
+    }
+
+    public static String getVideoUrl() {
+        String videoStorage = System.getProperty("videoStorage",
+                "https://selenoid.autotests.cloud/video/");
+        String sessionId = ((RemoteWebDriver) WebDriverRunner.getWebDriver())
+                .getSessionId().toString();
+        return videoStorage + sessionId + ".mp4";
+    }
+}
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,17 +1,43 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import com.github.javafaker.Faker;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.util.Locale;
+public class TestBase {
 
-public abstract class TestBase {
-    protected static final Faker faker = new Faker(new Locale("US"));
     @BeforeAll
-    static void setUp() {
-        Configuration.browserSize = "1920x1080";
+    static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+
+        Configuration.browser = System.getProperty("browser", "chrome");
+
+        String remoteDriverUrl = System.getProperty("remoteDriverUrl");
+        if (remoteDriverUrl != null && !remoteDriverUrl.isEmpty()) {
+            Configuration.remote = remoteDriverUrl;
+
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
+            Configuration.browserCapabilities = capabilities;
+        }
+
+        SelenideLogger.addListener("allure", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(false));
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.consoleLogs();
+        Attach.addVideo();
     }
 }
