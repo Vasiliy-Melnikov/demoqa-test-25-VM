c01130f2d40d3590ebe21ed809f9399f987d6b90
basil <vanderman789@gmail.com>
2025-12-27 23:42:12 +0300

    1

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -4,33 +4,43 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.util.Map;
-
-public class SingleTest {
+public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = System.getProperty("baseUrl","https://demoqa.com");
-        Configuration.browser = System.getProperty("browser","chrome");
-        Configuration.browserSize = System.getProperty("resolution","1920x1080");
-        Configuration.browserVersion = System.getProperty("browserVersion","127.0");
-        Configuration.timeout = 10000;
-        Configuration.remote = System.getProperty("remoteDriverUrl","https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+
+        String browserVersion = System.getProperty("browserVersion");
+        if (browserVersion != null && !browserVersion.isEmpty()) {
+            Configuration.browserVersion = browserVersion;
+        }
+
+        String remoteDriverUrl = System.getProperty(
+                "remoteDriverUrl",
+                "https://user1:1234@selenoid.autotests.cloud/wd/hub"
+        );
+        Configuration.remote = remoteDriverUrl;
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
     }
 
     @BeforeEach
-    void beforeSingle() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
+    void beforeEach() {
+        SelenideLogger.addListener("allure", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(false));
     }
 
     @AfterEach
@@ -39,4 +49,7 @@ public class SingleTest {
         Attach.pageSource();
         Attach.consoleLogs();
         Attach.addVideo();
+
+        SelenideLogger.removeListener("allure");
     }
+}
