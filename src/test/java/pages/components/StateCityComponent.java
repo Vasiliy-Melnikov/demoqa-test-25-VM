6582951b66942dbe611d0ed279723d462347f158
basil <vanderman789@gmail.com>
2025-12-29 22:34:50 +0300

    stabilize state/city selection

--- src/test/java/pages/components/StateCityComponent.java
+++ src/test/java/pages/components/StateCityComponent.java
@@ -2,34 +2,26 @@ package pages.components;
 
 import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Condition.enabled;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.executeJavaScript;
 
 public class StateCityComponent {
 
     private final SelenideElement stateDropdown = $("#state");
     private final SelenideElement cityDropdown = $("#city");
+    private final SelenideElement stateInput = $("#react-select-3-input");
+    private final SelenideElement cityInput = $("#react-select-4-input");
 
     public void selectState(String state) {
-        stateDropdown.scrollIntoView(true);
-        stateDropdown.shouldBe(visible, enabled);
-        executeJavaScript("arguments[0].click();", stateDropdown);
-
-        $(byText(state))
-                .shouldBe(visible)
-                .click();
+        stateDropdown.scrollIntoView(true).click();
+        stateInput.shouldBe(enabled)
+                .setValue(state)
+                .pressEnter();
     }
-
     public void selectCity(String city) {
-        cityDropdown.scrollIntoView(true);
-        cityDropdown.shouldBe(visible, enabled);
-        executeJavaScript("arguments[0].click();", cityDropdown);
-
-        $(byText(city))
-                .shouldBe(visible)
-                .click();
+        cityDropdown.scrollIntoView(true).click();
+        cityInput.shouldBe(enabled)
+                .setValue(city)
+                .pressEnter();
     }
 }
