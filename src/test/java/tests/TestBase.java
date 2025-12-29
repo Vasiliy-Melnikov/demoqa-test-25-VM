5ffe6394b3547c91dc1d65259aa5ecff3833aec4
basil <vanderman789@gmail.com>
2025-12-29 17:46:27 +0300

    add parameters

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -17,16 +17,27 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "131.0");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+
+        String remoteUrl = System.getProperty(
+                "remoteUrl",
+                "https://user1:1234@selenoid.autotests.cloud/wd/hub"
+        );
+
+        if (remoteUrl != null && !remoteUrl.isEmpty()) {
+            Configuration.remote = remoteUrl;
+
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", true,
+                    "enableVideo", true
+            ));
+            Configuration.browserCapabilities = capabilities;
+        }
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
