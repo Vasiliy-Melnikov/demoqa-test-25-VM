f3b4166430e2e02d2f2468924ae0164684472122
basil <vanderman789@gmail.com>
2025-12-30 11:55:35 +0300

    optimize

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,9 +6,7 @@ import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import util.JsUtils;
 
 import java.util.Map;
 
@@ -51,11 +49,6 @@ public class TestBase {
         }
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-    }
-
-    @BeforeEach
-    void removeAdsFromPage() {
-        JsUtils.removeAds();
     }
 
     @AfterEach
