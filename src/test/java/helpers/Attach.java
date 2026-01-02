acf4a091d44f74c052e2685a29225a3f90af0a3c
basil <vanderman789@gmail.com>
2026-01-02 23:35:15 +0300

    pass sessionId to video attachment and sync getVideoUrl signature

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -37,19 +37,19 @@ public class Attach {
     }
 
     @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
-    public static String addVideo() {
+    public static String addVideo(String sessionId) {
         return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
-                + getVideoUrl()
+                + getVideoUrl(sessionId)
                 + "' type='video/mp4'></video></body></html>";
     }
 
-    public static URL getVideoUrl() {
-        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4";
+    public static URL getVideoUrl(String sessionId) {
+        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId + ".mp4";
         try {
             return new URL(videoUrl);
         } catch (MalformedURLException e) {
             e.printStackTrace();
+            return null;
         }
-        return null;
     }
 }
