6c268dcee619428cbc165a358609894755f7b6f9
basil <vanderman789@gmail.com>
2026-01-03 00:09:13 +0300

    prevent test failures when browser logs are not supported (Firefox, Selenoid)

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.TakesScreenshot;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-
-import static com.codeborne.selenide.Selenide.sessionId;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.openqa.selenium.logging.LogType.BROWSER;
 
@@ -30,12 +28,19 @@ public class Attach {
     }
 
     public static void browserConsoleLogs() {
-        attachAsText(
-                "Browser console logs",
-                String.join("\n", Selenide.getWebDriverLogs(BROWSER))
-        );
+        try {
+            attachAsText(
+                    "Browser console logs",
+                    String.join("\n", Selenide.getWebDriverLogs(BROWSER))
+            );
+        } catch (Exception e) {
+            attachAsText(
+                    "Browser console logs",
+                    "Browser console logs are not available for this browser: "
+                            + e.getClass().getSimpleName()
+            );
+        }
     }
-
     @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
     public static String addVideo(String sessionId) {
         return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
